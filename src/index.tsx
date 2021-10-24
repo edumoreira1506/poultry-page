b07baca3072db34c0318f2b376adaa0eb37f821b
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-24 10:37:32 -0300

    Fix tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import Poultry from './views/Poultry/Poultry';
+
+(window as any).renderPoultryPage = (containerId: string) => {
+  const targetDocument = document.getElementById(containerId)
+
+  if (targetDocument) {
+    ReactDOM.render(
+      <Poultry />,
+      targetDocument,
+    )
+  }
+};
+
+(window as any).unmountPoultryPage = (containerId: string) => {
+  const targetDocument = document.getElementById(containerId)
+
+  if (targetDocument) {
+    ReactDOM.unmountComponentAtNode(targetDocument)
+  }
+}
