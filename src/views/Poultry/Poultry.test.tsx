ee8db907fb80da2493cda5a375989fbf290e6166
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-24 11:56:18 -0300

    Fix tests

--- src/views/Poultry/Poultry.test.tsx
+++ src/views/Poultry/Poultry.test.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import { poultryFactory } from '@cig-platform/factories'
 
 import Poultry from './Poultry'
 
+const DEFAULT_PROPS = {
+  poultry: poultryFactory(),
+  images: [],
+}
+
 describe('Poultry', () => {
   it('renders correctly', () => {
-    render(<Poultry />)
+    render(<Poultry {...DEFAULT_PROPS} />)
 
-    expect(screen.getByText('Poultry')).toBeInTheDocument()
+    expect(screen.getByText(DEFAULT_PROPS.poultry.name)).toBeInTheDocument()
+    expect(screen.getByText(DEFAULT_PROPS.poultry.description)).toBeInTheDocument()
   })
 })
