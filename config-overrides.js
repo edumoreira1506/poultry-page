75d22686b0e705fbc816735872a15384e9cf5d25
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-24 10:21:10 -0300

    Setup eslint

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,17 @@
 module.exports = {
   webpack: (config, env) => {
-    config.optimization.runtimeChunk = false;
+    config.optimization.runtimeChunk = false
     config.optimization.splitChunks = {
       cacheGroups: {
         default: false,
       },
-    };
+    }
 
-    config.output.filename = "static/js/[name].js";
+    config.output.filename = 'static/js/[name].js'
 
-    config.plugins[5].options.filename = "static/css/[name].css";
-    config.plugins[5].options.moduleFilename = () => "static/css/main.css";
+    config.plugins[5].options.filename = 'static/css/[name].css'
+    config.plugins[5].options.moduleFilename = () => 'static/css/main.css'
 
-    return config;
+    return config
   },
-};
+}
