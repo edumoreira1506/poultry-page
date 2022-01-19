15ae619a2025e0854e8660d56e40f0f3ecfcceef
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-19 19:58:57 -0300

    Add ContentSearchClient

---
+++ src/clients/ContentSearchClient.ts
@@ -0,0 +1,5 @@
+import ContentSearchClient from '@cig-platform/content-search-client'
+
+import { CONTENT_SEARCH_API_URL } from '../constants/url'
+
+export default new ContentSearchClient(CONTENT_SEARCH_API_URL)
